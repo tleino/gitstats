f980a95cced753dbedefb2f9748335e8cfd8e97d
Tommi Leino <namhas@gmail.com>
2021-07-27 19:40:42 +0300

    Distinguish LPC from C
    For this purpose sloccount was removed as it was easier to script
    the recognition of different files by custom scripting.

--- mkstatistics.sh
+++ mkstatistics.sh
@@ -1,44 +1,101 @@
 #!/bin/sh
 
-which sloccount >/dev/null || exit 1
+function sh_files {
+	for a in $(find $1 -type f -not -path \*/.git\*) ; do
+		if head -1 $a | grep '#!/bin/sh' >/dev/null ; then
+			echo $a
+		fi
+	done
+}
+
+function m4_files {
+	for a in $(find $1 \
+	    -type f \
+	    -not -path \*/.git\* \
+	    -path \*.m4 \
+	    -or -path \*.in \
+	    -or -path \*.ac) ; do
+		if file $a | grep 'M4 macro language' >/dev/null ; then
+			echo $a
+		fi
+	done
+}
+
+function cpp_files {
+	find $1 \
+		-not -path \*/.git\* \
+		-path \*[.]cpp \
+		-or -path \*[.]hpp \
+		-or -path \*[.]cc \
+		-or -path \*[.]cxx
+	for a in $(find $1 -type f \
+	    -not -path \*/.git\* \
+	    -path \*[.]h \
+	    -not -path \*majik?/lib\*) ; do
+		if head -1 $a | grep class >/dev/null ; then
+			echo $a
+		fi
+	done
+}
+
+function cpp_loc {
+	cat $(cpp_files $1) | wc -l
+}
+
+function shell_loc {
+	cat $(sh_files $1) | wc -l
+}
+
+function m4_loc {
+	cat $(m4_files $1) | wc -l
+}
+
 which git >/dev/null || exit 1
-which mktemp >/dev/null || exit 1
 
 GITDIR=/git
 PUBLISHED=/git/published
 CLONEDIR=/tmp/allgit
-if [ -x $CLONEDIR ] ; then
-	echo "Deleting $CLONEDIR ..."
-	sleep 2
-	rm -rf $CLONEDIR
+
+if [ ! -x $CLONEDIR ] ; then
+	mkdir $CLONEDIR
 fi
-mkdir $CLONEDIR
 for a in $(cat $PUBLISHED) ; do
 	(cd $CLONEDIR && git clone file://${GITDIR}/$a)
 done
-echo find /tmp/allgit -path \*/.git\* -delete
-sleep 2
-find /tmp/allgit -path \*/.git\* -delete
-TOTAL=$(ls -1 $CLONEDIR | wc -l)
-TMPFILE=$(mktemp /tmp/mkstatistics.XXXXXXXXX)
-(cd $CLONEDIR && sloccount --crossdups --addlangall . >/$TMPFILE 2>/dev/null)
+
+SHELL_LOC=$(shell_loc $CLONEDIR)
+CPP_LOC=$(cpp_loc $CLONEDIR)
+M4_LOC=$(m4_loc $CLONEDIR)
+
+C_LOC=$(cat $(find $CLONEDIR \
+	-path \*[.][ch] \
+	-not -path \*/.git\* \
+	-not -path \*majik?/lib\*) | wc -l)
+LPC_LOC=$(cat $(find $CLONEDIR \
+	-not -path \*/.git\* \
+	-path \*majik?/lib\*[.][ch]) | wc -l)
+JAVA_LOC=$(cat $(find $CLONEDIR \
+	-not -path \*/.git\* \
+	-path \*[.]java) | wc -l)
+AWK_LOC=$(cat $(find $CLONEDIR \
+	-not -path \*/.git\* \
+	-path \*[.]awk) | wc -l)
+
+TOTAL_LOC=$(expr $C_LOC + $LPC_LOC + $JAVA_LOC + \
+	$CPP_LOC + $SHELL_LOC + $AWK_LOC + $M4_LOC)
+
+echo "<p>"
+echo "Projects published: <b>$(ls -1 $CLONEDIR | wc -l)</b><br>"
+echo "Published lines of code: <b>\t$TOTAL_LOC</b><br>"
 echo "Statistics last updated: <b>$(date +'%B %d, %Y')</b><br>"
-echo "Published projects: <b>$TOTAL</b><br>"
-cat $TMPFILE \
-	| grep 'Total Physical' \
-	| sed -e 's/ *=/:/' -e 's/: \(.*\)/: <b>\1<\/b>/' -e 's/$/<br>/'
-cat $TMPFILE \
-	| grep 'Total Estimated Cost to Develop' \
-	| sed -e 's/ *=/:/' -e 's/: \(.*\)/: <b>\1<\/b> USD/' -e 's/$/<br>/' \
-		-e 's/\$//'
 echo "<pre>"
-cat $TMPFILE \
-	| sed -n -e '/Totals grouped by language/,/^$/p'
+echo "C\t<b>$C_LOC</b> lines"
+echo "C++\t<b>$CPP_LOC</b> lines"
+echo "LPC\t<b>$LPC_LOC</b> lines"
+echo "Java\t<b>$JAVA_LOC</b> lines"
+echo "Shell\t<b>$SHELL_LOC</b> lines"
+echo "AWK\t<b>$AWK_LOC</b> lines"
+echo "M4\t<b>$M4_LOC</b> lines"
 echo "</pre>"
-echo "<div class='credit'>"
-cat $TMPFILE \
-	| grep 'Please credit' \
-	| cut -d\" -f2- \
-	| sed 's/"//g' \
-	| sed 's/$/<br>/'
-echo "</div>"
+echo "</p>"
+

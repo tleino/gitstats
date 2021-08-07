385c1758b6ec07b837f25f574df74a232dfeaa96
Tommi Leino <namhas@gmail.com>
2021-08-07 09:59:15 +0300

    Add Javascript statistics

--- mkstatistics.sh
+++ mkstatistics.sh
@@ -77,6 +77,9 @@ LPC_LOC=$(cat $(find $CLONEDIR \
 JAVA_LOC=$(cat $(find $CLONEDIR \
 	-not -path \*/.git\* \
 	-path \*[.]java) | wc -l)
+JS_LOC=$(cat $(find $CLONEDIR \
+	-not -path \*/.git\* \
+	-path \*[.]js) | wc -l)
 HTML_LOC=$(cat $(find $CLONEDIR \
 	-not -path \*/.git\* \
 	-path \*[.]html) | wc -l)
@@ -89,22 +92,23 @@ AWK_LOC=$(cat $(find $CLONEDIR \
 	-path \*[.]awk) | wc -l)
 
 TOTAL_LOC=$(expr $C_LOC + $LPC_LOC + $JAVA_LOC + \
-	$CPP_LOC + $SHELL_LOC + $AWK_LOC + $M4_LOC + $HTML_LOC + $ROFF_LOC)
+	$CPP_LOC + $SHELL_LOC + $AWK_LOC + $M4_LOC + $HTML_LOC + $ROFF_LOC + \
+	$JS_LOC)
 
 echo "<p>"
 echo "Projects published: <b>$(ls -1 $CLONEDIR | wc -l)</b><br>"
 echo "Published lines of code: <b>\t$TOTAL_LOC</b><br>"
 echo "Last updated: <b>$(date +'%B %d, %Y')</b><br>"
 echo "<pre>"
-echo "C\t<b>$C_LOC</b> lines"
-echo "C++\t<b>$CPP_LOC</b> lines"
-echo "LPC\t<b>$LPC_LOC</b> lines"
-echo "Java\t<b>$JAVA_LOC</b> lines"
-echo "Shell\t<b>$SHELL_LOC</b> lines"
-echo "Roff\t<b>$ROFF_LOC</b> lines"
-echo "HTML\t<b>$HTML_LOC</b> lines"
-echo "AWK\t<b>$AWK_LOC</b> lines"
-echo "M4\t<b>$M4_LOC</b> lines"
+echo "C          <b>$C_LOC</b> lines"
+echo "C++        <b>$CPP_LOC</b> lines"
+echo "LPC        <b>$LPC_LOC</b> lines"
+echo "Javascript <b>$JS_LOC</b> lines"
+echo "Java       <b>$JAVA_LOC</b> lines"
+echo "Shell      <b>$SHELL_LOC</b> lines"
+echo "Roff       <b>$ROFF_LOC</b> lines"
+echo "HTML       <b>$HTML_LOC</b> lines"
+echo "AWK        <b>$AWK_LOC</b> lines"
+echo "M4         <b>$M4_LOC</b> lines"
 echo "</pre>"
 echo "</p>"
-
